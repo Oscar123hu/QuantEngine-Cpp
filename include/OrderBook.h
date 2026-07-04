2630ba189f567f922628f3103628ba8be15a8939
Oscar123hu <15180032335@163.com>
2026-07-04 20:40:52 +0800

    更新订单簿结构

--- include/OrderBook.h
+++ include/OrderBook.h
@@ -1,11 +1,12 @@
 #pragma once
-#include <iostream>
 #include <map>
 #include <list>
-#include <string>
+#include <cstdint>
 
+// 交易方向：买入或卖出
 enum class Side { BUY, SELL };
 
+// 单条订单基础数据
 struct Order {
     uint64_t orderId;
     double price;
@@ -13,12 +14,19 @@ struct Order {
     Side side;
 };
 
+// 限价订单簿核心类
 class OrderBook {
 private:
+    // 买单队列：价格从高到低排序 (std::greater)
     std::map<double, std::list<Order>, std::greater<double>> buyOrders;
+
+    // 卖单队列：价格从低到高排序 (std::less)
     std::map<double, std::list<Order>, std::less<double>> sellOrders;
 
 public:
+    // 提交新订单并触发撮合逻辑
     void submitOrder(Order order);
+
+    // 打印当前的买卖盘口数据
     void printBook() const;
 };
